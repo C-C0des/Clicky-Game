b465dd3b817cf70884f86759411803ddba4745fd
Diaspar <the.diaspar@gmail.com>
2018-12-15 01:08:52 -0500

    Updates

---
+++ precache-manifest.e092673b4b8276a8974e5c4c41f2a330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "8e7ff83c76692b5ded55",
+    "url": "/Clicky-Game/static/js/main.8e7ff83c.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/Clicky-Game/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "8e7ff83c76692b5ded55",
+    "url": "/Clicky-Game/static/css/main.9ab4a819.chunk.css"
+  },
+  {
+    "revision": "9860a18e6874a6d30da5ff1336833ca8",
+    "url": "/Clicky-Game/index.html"
+  }
+];
